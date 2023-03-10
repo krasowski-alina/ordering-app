3a91fbdbd0c540fea04e80ef3080ec7170a26e69
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-10 13:31:44 -0500

    used join method to add comma between items

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ import { menuArray } from '/data.js'
 function getFeedHtml(menuArray){
     let itemsHtml = ``
     menuArray.forEach(menuItem => {
-        let ingredientsStr = ``
-
-        menuItem.ingredients.forEach(ingredient => {
-            console.log(ingredient.length - 1)
-            return ingredientsStr += `${ingredient}, `
-        })
         itemsHtml += ` 
         <div class="main-container">
             <div class="main-container-items">
                <p class="icon">${menuItem.emoji}</p>
                <div class="items">
                     <h3 class="item-name">${menuItem.name}</h3>
-                    <p class="item-ingredients">${ingredientsStr}</p>
+                    <p class="item-ingredients">${menuItem.ingredients.join(', ')}</p>
                     <p class="item-price">$ ${menuItem.price}</p>
                </div>
             </div>
